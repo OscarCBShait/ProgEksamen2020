b6e6914602a5a2fd9aae279232348564a6daaaab
OscarCBShait <oscarpedersen1999@hotmail.com>
2020-11-24 12:03:09 +0100

    oprettet nye funktioner

--- Backend/server.js
+++ Backend/server.js
@@ -4,4 +4,4 @@ const server = express();
 
 server.use(cors())
 const port = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+server.listen(PORT, () => console.log(`Listening on port ${port}...`));
