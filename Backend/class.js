f57cd1103ab200a02f0a3cf2b28651c220562881
OscarCBShait <oscarpedersen1999@hotmail.com>
2020-11-24 12:49:14 +0100

    test af classer

--- Backend/class.js
+++ Backend/class.js
@@ -17,11 +17,11 @@ class User{
 }
 
 // i denne bid code hardcoder vi vores attributer for de to objekter
-Mads = new User("Mads", "Larsen", 18, "Hankøn", "Mads18", "hej12345", "mads@larsen.com", [Mille, Christian, Emilie], [Sofie], [Mille, Christian, Emilie])
-Mille = new User("Mille", "Hansen", 25, "Hunkøn", "Mille25", "hallo12345", "mille@gmail.com", [Mads, Sofie, Emilie], [Christian], [Mads, Sofie])
-Christian = new User("Christian", "Nielsen", 23, "Hankøn", "Christian23", "hi12345", "christian@hotmail.com", [Mads, Mille, Emilie], [Sofie], [Mads, Emilie])
-Emilie = new User("Emilie", "Mikkelsen", 19, "Hunkøn", "Emilie19", "EM1800", "emilie@yahoo.com", [Mads, Christian], [Mille, Sofie], [Mads, Christian])
-Sofie = new User("Sofie", "Svendsen", 22, "Hunkøn", "Mads18", "hej12345", "mads@larsen.com", [Mads, Mille, Christian, Emilie], [], [Mille])
+var Mads = new User("Mads", "Larsen", 18, "Hankøn", "Mads18", "hej12345", "mads@larsen.com", [Mille, Christian, Emilie], [Sofie], [Mille, Christian, Emilie])
+var Mille = new User("Mille", "Hansen", 25, "Hunkøn", "Mille25", "hallo12345", "mille@gmail.com", [Mads, Sofie, Emilie], [Christian], [Mads, Sofie])
+var Christian = new User("Christian", "Nielsen", 23, "Hankøn", "Christian23", "hi12345", "christian@hotmail.com", [Mads, Mille, Emilie], [Sofie], [Mads, Emilie])
+var Emilie = new User("Emilie", "Mikkelsen", 19, "Hunkøn", "Emilie19", "EM1800", "emilie@yahoo.com", [Mads, Christian], [Mille, Sofie], [Mads, Christian])
+var Sofie = new User("Sofie", "Svendsen", 22, "Hunkøn", "Mads18", "hej12345", "mads@larsen.com", [Mads, Mille, Christian, Emilie], [], [Mille])
 
 
 
@@ -29,6 +29,6 @@ Sofie = new User("Sofie", "Svendsen", 22, "Hunkøn", "Mads18", "hej12345", "mads
 
 // vi samler vores objekter i et array for dernæst at kunne eksportere begge objekter
 let ArrayUser = [Mads, Mille, Christian, Emilie, Sofie];
-
+console.log(ArrayUser)
 // module.exports gør, at vi kan eksportere vores array, og anvende det i andre js.filer
 module.exports = ArrayUser
