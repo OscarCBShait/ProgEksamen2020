04f496ea7179cd24fbde09043bfecde20a1d7e18
OscarCBShait <oscarpedersen1999@hotmail.com>
2020-11-24 13:07:21 +0100

    test af classer

--- Backend/server.js
+++ Backend/server.js
@@ -7,3 +7,10 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
+
+const ArrayUser = require("../Backend/class.js");
+
+// GET REQUEST = User // GET/read bruges til at læse ressourcer i vores REST-enviroment. GET request ændrer aldrig på informationen, men henter den blot
+app.get('/class', (req, res) => { // denne skal bruges til User
+    res.send(ArrayUser);
+});
