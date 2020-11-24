644a0b791b35caa3930f89a0ecf30daf333d414f
OscarCBShait <oscarpedersen1999@hotmail.com>
2020-11-24 12:44:20 +0100

    ny klasse

--- Backend/class.js
+++ Backend/class.js
@@ -17,11 +17,11 @@ class User{
 }
 
 // i denne bid code hardcoder vi vores attributer for de to objekter
-newuser1 = new User("Mads", "Larsen", 18, "Hankøn", "Mads18", "hej12345", "mads@larsen.com", ["Mille, Christian, Emilie, Sofie, Lone, Tobias"], ["Henrik, Hans, Mads"], ["Mille, Emilie, Christian, Sofie"])
-newuser2 = new User("Mille", "Hansen", 25, "Hunkøn", "Mads18", "hej12345", "mads@larsen.com", ["Mads, Christian, Emilie, Sofie, Lone, Tobias"], ["Henrik, Hans, Mads"], ["Mads, Emilie, Christian, Sofie"])
-newuser3 = new User("Christian", "Nielsen", 23, "Hankøn", "Mads18", "hej12345", "mads@larsen.com", ["Mille, Christian, Emilie, Sofie, Lone, Tobias"], ["Henrik, Hans, Mads"], ["Mille, Emilie, Christian, Sofie"])
-newuser4 = new User("Emilie", "Mikkelsen", 19, "Hunkøn", "Mads18", "hej12345", "mads@larsen.com", ["Mille, Christian, Emilie, Sofie, Lone, Tobias"], ["Henrik, Hans, Mads"], ["Mille, Emilie, Christian, Sofie"])
-newuser5 = new User("Sofie", "Svendsen", 22, "Hunkøn", "Mads18", "hej12345", "mads@larsen.com", ["Mille, Christian, Emilie, Sofie, Lone, Tobias"], ["Henrik, Hans, Mads"], ["Mille, Emilie, Christian, Sofie"])
+Mads = new User("Mads", "Larsen", 18, "Hankøn", "Mads18", "hej12345", "mads@larsen.com", [Mille, Christian, Emilie], [Sofie], [Mille, Christian, Emilie])
+Mille = new User("Mille", "Hansen", 25, "Hunkøn", "Mille25", "hallo12345", "mille@gmail.com", [Mads, Sofie, Emilie], [Christian], [Mads, Sofie])
+Christian = new User("Christian", "Nielsen", 23, "Hankøn", "Christian23", "hi12345", "christian@hotmail.com", [Mads, Mille, Emilie], [Sofie], [Mads, Emilie])
+Emilie = new User("Emilie", "Mikkelsen", 19, "Hunkøn", "Emilie19", "EM1800", "emilie@yahoo.com", [Mads, Christian], [Mille, Sofie], [Mads, Christian])
+Sofie = new User("Sofie", "Svendsen", 22, "Hunkøn", "Mads18", "hej12345", "mads@larsen.com", [Mads, Mille, Christian, Emilie], [], [Mille])
 
 
 
