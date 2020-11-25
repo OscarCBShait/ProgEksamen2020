d835762566448d7402b02f34d659a0f497fe6bc9
OscarCBShait <oscarpedersen1999@hotmail.com>
2020-11-25 10:40:51 +0100

    test af classer

---
+++ Backend/validation.js
@@ -0,0 +1,45 @@
+const email = document.getElementById('email')
+const password = document.getElementById('psw')
+const fornavn = document.getElementById('fn')
+const efternavn = document.getElementById('en')
+const alder = document.getElementById('alder')
+const brugernavn = document.getElementById('brugernavn')
+const køn = document.getElementById('køn')
+
+  
+
+password.addEventListener("onkeyup", myFunction);
+
+function myFunction() {
+    var message, password;
+    message = document.getElementById("pw");
+    message.innerHTML = "";
+    password = document.getElementById("psw").value.length;
+    try { 
+      if(password == "")  throw "udfyldes";
+     
+      password = Number(password);
+      if(password <= 8) throw "være mere end 8 tegn";
+      
+      if(15 >= password) throw "være mindre end 16 tegn";
+    
+    }
+    catch(err) {
+      message.innerHTML = "Password skal " + err;
+    }
+    efternavn = document.getElementById("psw").value.length;
+    try { 
+      if(password == "")  throw "udfyldes";
+     
+      password = Number(password);
+      if(password <= 8) throw "være mere end 8 tegn";
+      
+      if(15 >= password) throw "være mindre end 16 tegn";
+    
+    }
+    catch(err) {
+      message.innerHTML = "Password skal " + err;
+    }
+  }
+
+ 
