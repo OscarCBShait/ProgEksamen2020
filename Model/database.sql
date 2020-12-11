324a41ff30428e26650a63404571dd9899bcc8df
OscarCBShait <oscarpedersen1999@hotmail.com>
2020-12-11 01:36:24 +0100

    sidste sql

--- Model/database.sql
+++ Model/database.sql
@@ -7,8 +7,18 @@ CREATE TABLE IF NOT EXISTS users (
   bio varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS matchlist (
+  id int(11) NOT NULL,
+  user_id int NOT NULL,
+  match_user_id int NOT NULL,
+  is_a_match int NOT NULL,
+  user_name varchar(45) NOT NULL,
+  match_user_name varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-Se bilag 6, hvor jeg lavet mit ER-diagram
+
+Se ligeledes bilag 6, hvor jeg lavet mit ER-diagram
 
 
 
